600f495d7a022a8f88943b75e9117c22db2d84be
kkyparkk <kkyparkk@gmail.com>
2025-08-19 22:28:55 +0900

    [Bronze V] Title: 윤년, Time: 176 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        //윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+        if(N%4==0 && N%100!=0 || N%400==0){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+    }
+}
