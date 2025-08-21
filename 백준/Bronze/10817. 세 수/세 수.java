83c0b426f3a9a76b675f57627f8d90acabde89be
kkyparkk <kkyparkk@gmail.com>
2025-08-21 22:17:43 +0900

    [Bronze IV] Title: 세 수, Time: 176 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
